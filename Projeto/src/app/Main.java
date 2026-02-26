d3fe72cab303522bd5fda4f7c72b4e6ccb1d92ae
Julio Morais <eujuliomateusmorais@gmail.com>
2026-02-26 20:31:45 -0300

    feat(projeto-modulo-basico): implementa tratamento de exceção

--- Projeto/src/app/Main.java
+++ Projeto/src/app/Main.java
@@ -18,50 +18,49 @@ public class Main {
         String descricao;
         boolean concluida = false;
 
-        while(true){
-            System.out.println(
-                    "\n=====GERENCIADOR DE TAREFAS=====" +
-                            "\n1. Criar nova tarefa" +
-                            "\n2. Listar tarefas" +
-                            "\n3. Marcar tarefa como concluída" +
-                            "\n4. Remover tarefa" +
-                            "\n5. Sair" +
-                            "\nEscolha uma opção:"
-            );
-            opcao = scanner.nextInt();
-            scanner.nextLine();
+        try {
+            while (true) {
+                System.out.println(
+                        "\n=====GERENCIADOR DE TAREFAS=====" +
+                                "\n1. Criar nova tarefa" +
+                                "\n2. Listar tarefas" +
+                                "\n3. Marcar tarefa como concluída" +
+                                "\n4. Remover tarefa" +
+                                "\n5. Sair" +
+                                "\nEscolha uma opção:"
+                );
+                opcao = scanner.nextInt();
+                scanner.nextLine();
 
-            if(opcao == 1){
-                incrementeID += 1;
+                if (opcao == 1) {
+                    incrementeID += 1;
 
-                System.out.println("Digite o título da nova tarefa:");
-                titulo = scanner.nextLine();
+                    System.out.println("Digite o título da nova tarefa:");
+                    titulo = scanner.nextLine();
 
-                System.out.println("Digite a descrição da tarefa");
-                descricao = scanner.nextLine();
+                    System.out.println("Digite a descrição da tarefa");
+                    descricao = scanner.nextLine();
 
-                listaTarefas.adicionarTarefa(incrementeID, titulo, descricao, concluida);
+                    listaTarefas.adicionarTarefa(incrementeID, titulo, descricao, concluida);
+                } else if (opcao == 2) {
+                    listaTarefas.listarTarefas();
+                } else if (opcao == 3) {
+
+                } else if (opcao == 4) {
+                    int removerID = 0;
+                    System.out.println("\nDigite o ID da tarefa a ser removida");
+                    removerID = scanner.nextInt();
+                    listaTarefas.removerTarefa(removerID);
+                } else if (opcao == 5) {
+                    System.out.println("\nPrograma encerrado!");
+                    break;
+                } else {
+                    System.out.println("Digite uma opção válida");
+                }
             }
-
-            else if(opcao == 2){
-                listaTarefas.listarTarefas();
-            }
-
-            else if(opcao == 3){
-
-            }
-
-            else if(opcao == 4){
-                int removerID = 0;
-                System.out.println("\nDigite o ID da tarefa a ser removida");
-                removerID = scanner.nextInt();
-                listaTarefas.removerTarefa(removerID);
-            }
-
-            else if(opcao == 5){
-                System.out.println("\nPrograma encerrado!");
-                break;
-            }
+        }
+        catch(Exception e){
+            System.out.println("Você digitou uma entrada inválida. Por essa razão o programa foi finalizado!");
         }
     }
 
